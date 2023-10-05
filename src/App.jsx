1ab1d1f02f94fba5bb60f8cd74a06461d89c78e6
Nitin Singh <vortechcorp@gmail.com>
2023-10-05 23:24:10 +0530

    added auth feature

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import { AppRouter } from './routes';
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <AppRouter/>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
